776005cb09395c07835f6ea84990e4524d8e3d95
Dorotka <dorotamamczak@gmail.com>
2021-08-25 13:52:35 +0200

    added a bubble generator

--- src/components/Button/Button.jsx
+++ src/components/Button/Button.jsx
@@ -1,4 +1,4 @@
-import React, { useRef, useState } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import './Button.scss';
 
 
@@ -8,22 +8,112 @@ const Button = () => {
 
     const refWrapButton = useRef(null);
     const [txTAnimation, setTxtAnimation] = useState('Search & Hire');
+    const [start, setStart] = useState(false);
+
+
+    const createBubbles = () => {
+        const NUM_OF_BUBBLES = 100;
+        const bubbles = [];
+        for (let i = 0; i < NUM_OF_BUBBLES; i++) {
+            const span = <span className={`bubbles`} key={bubbles.length}></span>;
+            bubbles.push(span);
+        }
+        return bubbles;
+    }
+
+    useEffect(() => {
+        if (start) {
+
+            if (!refWrapButton.current.classList.contains('clicked')) {
+                setTimeout(() => {
+                    refWrapButton.current.classList.remove('clicked');
+                    setStart(false);
+                    setTxtAnimation('Search & Hire');
+                }, 3000);
+                const getAllBubbles = [...document.querySelectorAll('span.bubbles')];
+
+                getAllBubbles.map(bubble => {
+                    const generateRandomPositionX = Math.random() * (100 - (0)) + (0);
+                    const generateRandomPositionY = Math.random() * (100 - (0)) + (0);
+                    const generateDelay = Math.random() * (1000 - (0)) + (0);
+                    const time = Math.random() * (1000 - (500)) + (500);
+
+                    bubble.animate([
+                        {
+                            transform: 'scale(1.1) translate(-50%,-50%) perspective(800px)',
+                            visibility: 'visible',
+                            left: '50%',
+                            top: '50% '
+                        },
+                        {
+                            transform: 'scale(0) translate(-50%,-50%) perspective(800px)',
+                            visibility: 'hidden',
+                            left: `${generateRandomPositionX}%`,
+                            top: `${generateRandomPositionY}%`
+                        }
+                    ], {
+                        duration: time,
+                        easing: "linear",
+                        delay: generateDelay,
+
+                    })
+                });
+            };
+            refWrapButton.current.classList.add('clicked');
+
+        }
+
+    }, [start])
 
     const startAnimation = () => {
-        if (!refWrapButton.current.classList.contains('clicked')) {
-            setTimeout(() => {
-                refWrapButton.current.classList.remove('clicked');
-                console.log(refWrapButton.current, 'can click')
-            }, 3000);
-        };
-        refWrapButton.current.classList.add('clicked');
+        setStart(true);
+        setTxtAnimation('searching ...');
+        // console.log(start)
+        // if (!refWrapButton.current.classList.contains('clicked')) {
+        //     setTimeout(() => {
+        //         refWrapButton.current.classList.remove('clicked');
+        //         setStart(false);
+        //     }, 3000);
+        //     const getAllBubbles = [...document.querySelectorAll('span.bubbles')];
 
+        //     getAllBubbles.map(bubble => {
+        //         const generateRandomPositionX = Math.random() * (100 - (0)) + (0);
+        //         const generateRandomPositionY = Math.random() * (100 - (0)) + (0);
+        //         const generateDelay = Math.random() * (1000 - (0)) + (0);
+        //         const time = Math.random() * (1000 - (500)) + (500);
+
+        //         bubble.animate([
+        //             {
+        //                 transform: 'scale(1.1) translate(-50%,-50%) perspective(800px)',
+        //                 visibility: 'visible',
+        //                 left: '50%',
+        //                 top: '50% '
+        //             },
+        //             {
+        //                 transform: 'scale(0) translate(-50%,-50%) perspective(800px)',
+        //                 visibility: 'hidden',
+        //                 left: `${generateRandomPositionX}%`,
+        //                 top: `${generateRandomPositionY}%`
+        //             }
+        //         ], {
+        //             duration: time,
+        //             easing: "linear",
+        //             delay: generateDelay,
+
+        //         })
+        //     });
+
+        //     setTxtAnimation('searching ...');
+        // };
+        // refWrapButton.current.classList.add('clicked');
     }
 
 
     return (
         <div className="wrap" ref={refWrapButton}>
-            <button onClick={startAnimation} className='btn'>{txTAnimation}</button>
+            <button onClick={startAnimation}
+                className='btn'>{txTAnimation}</button>
+            <div className='bubbles-wrap'>{start ? createBubbles() : null}</div>
         </div>
     )
 }
