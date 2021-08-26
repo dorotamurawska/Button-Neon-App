5994dfaf4df555bd1a3ea121f93eaa6225993903
Dorotka <dorotamamczak@gmail.com>
2021-08-26 12:19:53 +0200

    refactored components:  Button, App

--- src/App.js
+++ src/App.js
@@ -1,13 +1,38 @@
-import React from 'react';
-import './App.css';
+import React, { useState, useRef } from 'react';
+import './App.scss';
 import Button from './components/Button/Button';
+import Description from './components/Description/Description';
+import Bubbles from './components/Bubbles/Bubbles';
 
 const App = () => {
 
+  const [start, setStart] = useState(false);
+  const [txtAnimation, setTxtAnimation] = useState('Search & Hire');
+
+  const refWrapButton = useRef(null);
+
   return (
     <div className="App" >
-      <h1>press the button to search and hire a new junior front end developer (react)</h1>
-      <Button />
+      <div
+        className="wrap"
+        ref={refWrapButton}
+      >
+        <Button
+          start={start}
+          setStart={setStart}
+          txtAnimation={txtAnimation}
+          setTxtAnimation={setTxtAnimation}
+        />
+        <Description
+          txtAnimation={txtAnimation}
+        />
+      </div>
+      <Bubbles
+        start={start}
+        setStart={setStart}
+        txtAnimation={txtAnimation}
+        setTxtAnimation={setTxtAnimation}
+        refWrapButton={refWrapButton} />
     </div >
   );
 }
--- src/components/Button/Button.jsx
+++ src/components/Button/Button.jsx
@@ -1,121 +1,18 @@
-import React, { useEffect, useRef, useState } from 'react';
+import React from 'react';
 import './Button.scss';
 
 
-
-
-const Button = () => {
-
-    const refWrapButton = useRef(null);
-    const [txTAnimation, setTxtAnimation] = useState('Search & Hire');
-    const [start, setStart] = useState(false);
-
-
-    const createBubbles = () => {
-        const NUM_OF_BUBBLES = 100;
-        const bubbles = [];
-        for (let i = 0; i < NUM_OF_BUBBLES; i++) {
-            const span = <span className={`bubbles`} key={bubbles.length}></span>;
-            bubbles.push(span);
-        }
-        return bubbles;
-    }
-
-    useEffect(() => {
-        if (start) {
-
-            if (!refWrapButton.current.classList.contains('clicked')) {
-                setTimeout(() => {
-                    refWrapButton.current.classList.remove('clicked');
-                    setStart(false);
-                    setTxtAnimation('Search & Hire');
-                }, 3000);
-                const getAllBubbles = [...document.querySelectorAll('span.bubbles')];
-
-                getAllBubbles.map(bubble => {
-                    const generateRandomPositionX = Math.random() * (100 - (0)) + (0);
-                    const generateRandomPositionY = Math.random() * (100 - (0)) + (0);
-                    const generateDelay = Math.random() * (1000 - (0)) + (0);
-                    const time = Math.random() * (1000 - (500)) + (500);
-
-                    bubble.animate([
-                        {
-                            transform: 'scale(1.1) translate(-50%,-50%) perspective(800px)',
-                            visibility: 'visible',
-                            left: '50%',
-                            top: '50% '
-                        },
-                        {
-                            transform: 'scale(0) translate(-50%,-50%) perspective(800px)',
-                            visibility: 'hidden',
-                            left: `${generateRandomPositionX}%`,
-                            top: `${generateRandomPositionY}%`
-                        }
-                    ], {
-                        duration: time,
-                        easing: "linear",
-                        delay: generateDelay,
-
-                    })
-                });
-            };
-            refWrapButton.current.classList.add('clicked');
-
-        }
-
-    }, [start])
+const Button = ({ setStart, txtAnimation, setTxtAnimation }) => {
 
     const startAnimation = () => {
         setStart(true);
-        setTxtAnimation('searching ...');
-        // console.log(start)
-        // if (!refWrapButton.current.classList.contains('clicked')) {
-        //     setTimeout(() => {
-        //         refWrapButton.current.classList.remove('clicked');
-        //         setStart(false);
-        //     }, 3000);
-        //     const getAllBubbles = [...document.querySelectorAll('span.bubbles')];
-
-        //     getAllBubbles.map(bubble => {
-        //         const generateRandomPositionX = Math.random() * (100 - (0)) + (0);
-        //         const generateRandomPositionY = Math.random() * (100 - (0)) + (0);
-        //         const generateDelay = Math.random() * (1000 - (0)) + (0);
-        //         const time = Math.random() * (1000 - (500)) + (500);
-
-        //         bubble.animate([
-        //             {
-        //                 transform: 'scale(1.1) translate(-50%,-50%) perspective(800px)',
-        //                 visibility: 'visible',
-        //                 left: '50%',
-        //                 top: '50% '
-        //             },
-        //             {
-        //                 transform: 'scale(0) translate(-50%,-50%) perspective(800px)',
-        //                 visibility: 'hidden',
-        //                 left: `${generateRandomPositionX}%`,
-        //                 top: `${generateRandomPositionY}%`
-        //             }
-        //         ], {
-        //             duration: time,
-        //             easing: "linear",
-        //             delay: generateDelay,
-
-        //         })
-        //     });
-
-        //     setTxtAnimation('searching ...');
-        // };
-        // refWrapButton.current.classList.add('clicked');
+        setTxtAnimation('Searching ...');
     }
 
-
-    return (
-        <div className="wrap" ref={refWrapButton}>
-            <button onClick={startAnimation}
-                className='btn'>{txTAnimation}</button>
-            <div className='bubbles-wrap'>{start ? createBubbles() : null}</div>
-        </div>
-    )
+    return <button
+                onClick={startAnimation}
+                className='btn'
+            >{txtAnimation}</button>
 }
 
 export default Button;
